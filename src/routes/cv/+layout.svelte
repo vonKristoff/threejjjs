69871d7b02bbd259b26f8a28af15e308b4408eec
JCEEE <0xjceee@proton.me>
2025-10-01 13:12:51 +0100

    add cv page

---
+++ src/routes/cv/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	let { children } = $props();
+</script>
+
+<section class="grid place-content-center my-24">
+	<article class="prose">
+		{@render children()}
+	</article>
+</section>
+
+<style>
+	:global(:root) {
+		--html-bg: white !important;
+	}
+</style>
