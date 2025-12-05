ed14d1941965736b3ff8edf58209655cc0e7a1a0
JCEEE <0xjceee@proton.me>
2025-12-05 11:30:47 +0000

    add new page

---
+++ src/routes/terra/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  let { children } = $props();
+</script>
+
+<section id="cv" class="grid place-content-center my-24 px-4">
+  <article class="prose w-full">
+    {@render children()}
+  </article>
+</section>
